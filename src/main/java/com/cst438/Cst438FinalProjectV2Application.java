2690cc15f42cc5986dbde4b4c0df2fc897278bc4
Juan Ansaldo <131718774+juanansaldo@users.noreply.github.com>
2023-11-18 14:11:08 -0800

    Add files via upload

---
+++ src/main/java/com/cst438/Cst438FinalProjectV2Application.java
@@ -0,0 +1,13 @@
+package com.cst438;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cst438FinalProjectV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cst438FinalProjectV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cst438/Cst438FinalProjectV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cst438;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cst438FinalProjectV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
