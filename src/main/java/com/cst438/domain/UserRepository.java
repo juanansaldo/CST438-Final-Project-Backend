a9a99d3fdfdf76baf4c27804cf69829b1af4bf21
Fabian Santano <94444784+FabianSantano@users.noreply.github.com>
2023-11-22 19:36:38 -0800

    Add files via upload

--- src/main/java/com/cst438/domain/UserRepository.java
+++ src/main/java/com/cst438/domain/UserRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.repository.query.Param;
 
 
 public interface UserRepository extends CrudRepository<User, Integer> {
-
+	@Query("SELECT u FROM User u WHERE u.username = :username")
     User findByUsername(String username);
 	  
     @Query("SELECT u FROM User u WHERE u.id = :userId")
